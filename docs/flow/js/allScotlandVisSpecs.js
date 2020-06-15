1f53819e5797c7047a1be445fe7489dc35107836
Jo Wood <jwo@soi.city.ac.uk>
2020-06-16 00:33:38 +0100

    Correct DOM id

--- docs/flow/js/allScotlandVisSpecs.js
+++ docs/flow/js/allScotlandVisSpecs.js
@@ -50,4 +50,4 @@ let vlLaMap = {
 // Reference each of the specs with an ID that can be used in the main HTML.
 // If a new spec is added above, add its name along with a corresponding DOM id.
 
-vegaEmbed("#laMap", vlLaMAp).catch(console.error);
+vegaEmbed("#laMap", vlLaMap).catch(console.error);
