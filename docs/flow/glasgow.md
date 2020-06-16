9d4889b5bd7ef5b323776420aa466be5fe0f5c08
Jo Wood <jwo@soi.city.ac.uk>
2020-06-16 01:33:09 +0100

    Add grid map to web page

--- docs/flow/glasgow.md
+++ docs/flow/glasgow.md
@@ -7,11 +7,17 @@
 
 ## 1. MSOA regions
 
-In contrast to the [all Scotland visualizations](#allScotland.md), we can examine the home-work flows at a more local scale by considering patterns aggregated at MSOA level, here for the Glasgow and Strathclyde region.
+In contrast to the [all Scotland visualizations](allScotland.md), we can examine the home-work flows at a more local scale by considering patterns aggregated at MSOA level, here for the Glasgow and Strathclyde region.
 
 <div class="wide" id="ggMap"></div>
 
 The challenge here is that we have many more regions (MSOAs) that we need to consider. As [previously](#allScotland.md) we can project each MSOA into a gridded location that attempts to preserve key geographic and topologic structure (_move mouse over cells to identify the MSOA_).
+
+<div class="wide" id="ggGridMap"></div>
+
+## 2. Flows
+
+Overlaying flow lines on the conventional geography leads to a 'hairball' with longer flows dominating the view hiding shorter flows. For assessing the travel pattern models, this is problematic. We can ameliorate this to some extend by filtering by length or volume of flow:
 
 <!-- Script containing the vis specs used above. Must be at end of document. -->
 <script src="js/glasgowVisSpecs.js"></script>
--- docs/flow/js/glasgowVisSpecs.js
+++ docs/flow/js/glasgowVisSpecs.js
@@ -50,8 +50,74 @@ let vlGgMap = {
   },
 };
 
+let ggGridMap = {
+  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  config: {
+    view: {
+      stroke: "",
+    },
+  },
+  background: "#f9f9f9",
+  width: 600,
+  height: 600,
+  data: {
+    url: "https://gicentre.github.io/scrc/data/grid/greaterGlasgowGrid.csv",
+  },
+  layer: [
+    {
+      encoding: {
+        x: {
+          field: "gridX",
+          type: "quantitative",
+          scale: {
+            nice: false,
+            domain: [0, 18],
+          },
+          axis: null,
+        },
+        y: {
+          field: "gridY",
+          type: "quantitative",
+          sort: "descending",
+          scale: {
+            nice: false,
+            domain: [0, 18],
+          },
+          axis: null,
+        },
+        color: {
+          field: "council",
+          type: "nominal",
+          scale: {
+            scheme: "tableau20",
+          },
+          legend: null,
+        },
+        tooltip: [
+          {
+            field: "regionName",
+            type: "nominal",
+          },
+          {
+            field: "council",
+            type: "nominal",
+          },
+        ],
+      },
+      mark: {
+        type: "square",
+        stroke: "white",
+        strokeWidth: 2,
+        opacity: 1,
+        size: 1111.111111111111,
+      },
+    },
+  ],
+};
+
 // -----------------------------------------------------------------------------
 // Reference each of the specs with an ID that can be used in the main HTML.
 // If a new spec is added above, add its name along with a corresponding DOM id.
 
 vegaEmbed("#ggMap", vlGgMap).catch(console.error);
+vegaEmbed("#ggGridMap", vlGgGridMap).catch(console.error);
